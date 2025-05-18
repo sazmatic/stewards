c9304435ec6e520f088cf28c7bb9b8dfba8d51b1
sazmatic <s.mikho@icloud.com>
2025-05-18 13:26:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -123,12 +123,12 @@ const representatives = {
     { name: "Tracy Barr", unit: "RN", email: "morkielu@yahoo.com" },
   ],
   "Southfield": [
-    { name: "Amy Pinneo", unit: "RN", phone: "313-445-0710", email: "helmamy13@gmail.com" },
-    { name: "Jana Nicol Weinert", unit: "RN", email: "Jnb904@hotmail.com" },
-    { name: "Katie Wilson", unit: "RN", site: "Flex RN", phone: "313-400-6424", email: "kwilso65@emich.edu" },
+    { name: "Amy Pinneo", shift: "Day", unit: "RN", phone: "313-445-0710", email: "helmamy13@gmail.com" },
+    { name: "Jana Nicol Weinert", shift: "Day", unit: "RN", email: "Jnb904@hotmail.com" },
+    { name: "Katie Wilson", shift: "Night", unit: "RN", site: "Flex RN", phone: "313-400-6424", email: "kwilso65@emich.edu" },
     { name: "Lindsey Paulick", unit: "RN", phone: "248-245-6273", email: "lindseypaulickRN@hotmail.com" },
     { name: "Meredith Corsetti", shift: "Night", unit: "RN", phone: "248-376-7712", email: "millermeredith@hotmail.com" },
-    { name: "Misty Gorman", unit: "RN", email: "misty12472@yahoo.com" },
+    { name: "Misty Gorman", shift: "Day", unit: "RN", email: "misty12472@yahoo.com" },
     { name: "Roseanne Ritzman", unit: "RN", email: "ritzman1123@gmail.com" },
   ],
   "Taylor": [
