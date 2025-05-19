0277f537a92d6c70097b30418a54d9b891115dc1
sazmatic <s.mikho@icloud.com>
2025-05-19 12:58:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const representatives = {
     { name: "Emmy Castillon", shift: "Day", unit: "RN", email: "emmycastillon1@gmail.com" },
     { name: "Jacquelyn Whitmore", shift: "Day", unit: "RN", phone: "734-837-3291", email: "Jczarn01@outlook.com" },
     { name: "Jill Judkins", shift: "Day", unit: "RN", phone: "248-224-1494", email: "timjudkins@comcast.net" },
+    { name: "John P. Denomy", shift: "Day", unit: "RN", email: "jpdenomy2@gmail.com" },
     { name: "Judie Laverdiere", shift: "Day", unit: "RN", email: "Judie.laverdiere@gmail.com" },
     { name: "Judy Hayes", shift: "Day", unit: "RN", email: "Lucykayla2@yahoo.com" },
     { name: "Julie Hicks", shift: "Day", phone: "248-840-0325", unit: "RN", email: "Juliemhicks123@yahoo.com" },
